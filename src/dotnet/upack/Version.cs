d2c2631551d3fe5b07901dfe1bd0bbfcd900bc69
Martin Gemme <Martin.Gemme@equisoft.com>
2019-08-08 13:44:41 -0400

    Adding the version command

---
+++ src/dotnet/upack/Version.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Inedo.UPack.CLI
+{
+    [DisplayName("version")]
+    [Description("Outputs the installed version of upack.")]
+    public class Version : Command
+    {
+        public override Task<int> RunAsync(CancellationToken cancellationToken)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            var version = fvi.FileVersion;
+
+            Console.WriteLine(version);
+
+            return Task.FromResult(0);
+        }
+    }
+}
